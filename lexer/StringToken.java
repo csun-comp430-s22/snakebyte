febc8c736cc72897ab148ba91d641fecede6ee31
katalla1 <katalla@hotmal.com>
2022-02-25 08:39:37 -0800

    updated tokens

--- lexer/StringToken.java
+++ lexer/StringToken.java
@@ -16,3 +16,27 @@ public class StringToken implements Token {
     }
 
 }
+/*
+ * commented out, will need this code later for the compiler
+ * public class StringToken implements Token {
+ * public final String value;
+ * 
+ * public StringToken(String value){
+ * 
+ * this.value=value;
+ * }
+ * public int hashCode(){
+ * return 10;
+ * }
+ * 
+ * public boolean equals (final Object other){
+ * return other instanceof StringToken;
+ * }
+ * 
+ * public String toString(){
+ * 
+ * return value;
+ * }
+ * 
+ * }
+ */
