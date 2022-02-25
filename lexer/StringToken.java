37e5c30a18234b40df95a1a06f37c7c427c21d2a
katalla1 <katalla@hotmal.com>
2022-02-25 08:27:52 -0800

    fixed string token

--- lexer/StringToken.java
+++ lexer/StringToken.java
@@ -1,23 +1,18 @@
 package lexer;
 
 public class StringToken implements Token {
-    public final String value;
 
-    public StringToken(String value){
-
-        this.value=value;
-    }
-    public int hashCode(){
+    public int hashCode() {
         return 10;
     }
 
-    public boolean equals (final Object other){
+    public boolean equals(final Object other) {
         return other instanceof StringToken;
     }
 
-    public String toString(){
+    public String toString() {
 
-        return value;
+        return "Str";
     }
-    
+
 }
