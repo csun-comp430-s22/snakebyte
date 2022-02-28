e3841490f6245a8c9f1e367ef536bdf8cfe57806
RuitaoWu <ruitaowu@yahoo.com>
2022-02-28 11:12:23 -0800

    add throw Exception to test file, but is not working yet

--- test/TokenizerTest.java
+++ test/TokenizerTest.java
@@ -1,8 +1,9 @@
 package lexer;
-import org.junit.Test.Assert.assertEquals;
-public class TokenizerTest {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+public class TokenizerTest throws TokennizerException {
     @Test
-    public void testTrue() {
+    public void testTrue() throws TokennizerException{
         Tokenizer tokenizer = new Tokenizer("true");
         assertEquals(1, tokenizer.getOffset());
         Assert.assertTrue(tokenizer instanceof TrueToken);
