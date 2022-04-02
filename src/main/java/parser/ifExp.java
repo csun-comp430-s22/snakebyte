9bfd4f816afb287d24823578ebf6596d7bd1e9bb
RuitaoWu <ruitaowu@yahoo.com>
2022-04-02 10:31:30 -0700

    fixed ifExp to IfExp

--- src/main/java/parser/ifExp.java
+++ src/main/java/parser/ifExp.java
@@ -1,19 +1,19 @@
 package parser;
 
 
-public class ifExp implements Expression{
+public class IfExp implements Expression{
     public final Expression gaurd;
     public final Expression truebranch;
     public final Expression falsebranch;
     
-    public ifExp(final Expression Guard,final Expression trueBranch,final Expression falseBranch){
+    public IfExp(final Expression Guard,final Expression trueBranch,final Expression falseBranch){
         this.gaurd= Guard;
         this.truebranch=trueBranch;
         this.falsebranch= falseBranch;
     }
     public boolean equals(final Object other) {
-        if (other instanceof ifExp) {
-            final ifExp otherif = (ifExp)other;
+        if (other instanceof IfExp) {
+            final IfExp otherif = (IfExp)other;
             return (gaurd.equals(otherif.gaurd) &&
                     truebranch.equals(otherif.truebranch) &&
                     falsebranch.equals(otherif.falsebranch));
