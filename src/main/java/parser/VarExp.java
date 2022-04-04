a8a268245560ada42ea0aabcd0813a2344d74b19
RuitaoWu <ruitaowu@yahoo.com>
2022-04-04 10:20:21 -0700

    updated VarExp.java

--- src/main/java/parser/VarExp.java
+++ src/main/java/parser/VarExp.java
@@ -3,23 +3,38 @@ package parser;
 
 
 public class VarExp  implements Expression{
-
-    public final String name;
-
-    public  VarExp(final String name) {
-        this.name = name;
+    //new modified code:
+    public final Var variable;
+    public VarExp(final Var variable){
+        this.variable = variable;
     }
-
-    public boolean equals(final Object other) {
+    public boolean equals(final Object other){
         return (other instanceof VarExp &&
-                name.equals(((VarExp)other).name));
+                variable.equals(((VarExp)other).variable));
     }
-
-    public int hashCode() {
-        return name.hashCode();
+    public int hashCode(){
+        return variable.hashCode();
     }
+    public String toString(){
+        return "VarExp(" + variable + ")";
+    }
+    //original code was:
+    // public final String name;
 
-    public String toString() {
-        return "VarExp(" + name + ")";
-    } 
+    // public  VarExp(final String name) {
+    //     this.name = name;
+    // }
+
+    // public boolean equals(final Object other) {
+    //     return (other instanceof VarExp &&
+    //             name.equals(((VarExp)other).name));
+    // }
+
+    // public int hashCode() {
+    //     return name.hashCode();
+    // }
+
+    // public String toString() {
+    //     return "VarExp(" + name + ")";
+    // } 
 }
