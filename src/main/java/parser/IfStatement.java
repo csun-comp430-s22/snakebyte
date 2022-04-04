b98a3cd689b1e7ab5ebefb449708318464f338d3
katalla1 <katalla@hotmail.com>
2022-04-03 19:27:35 -0700

    fixed spellings

--- src/main/java/parser/IfStatement.java
+++ src/main/java/parser/IfStatement.java
@@ -1,33 +1,38 @@
 
 package parser;
-public class IfStatement implements Statement{
-    public final Expression gaurd;
-    public final Statement truebranch;
-    public final Statement falsebranch;
-    public IfStatement(final Expression Guard,final Statement trueBranch,final Statement falseBranch){
-        this.gaurd= Guard;
-        this.truebranch=trueBranch;
-        this.falsebranch= falseBranch;
+
+public class IfStatement implements Statement {
+    public final Expression guard;
+    public final Statement trueBranch;
+    public final Statement falseBranch;
+
+    public IfStatement(final Expression guard, final Statement trueBranch, final Statement falseBranch) {
+        this.guard = guard;
+        this.trueBranch = trueBranch;
+        this.falseBranch = falseBranch;
     }
+
     public boolean equals(final Object other) {
-        if(other instanceof IfStatement){
-            final IfStatement otherif = (IfStatement)other;
-            return (gaurd.equals(otherif.gaurd) &&
-                    truebranch.equals(otherif.truebranch) &&
-                    falsebranch.equals(otherif.falsebranch));
-        }else{
+        if (other instanceof IfStatement) {
+            final IfStatement otherif = (IfStatement) other;
+            return (guard.equals(otherif.guard) &&
+                    trueBranch.equals(otherif.trueBranch) &&
+                    falseBranch.equals(otherif.falseBranch));
+        } else {
             return false;
         }
     }
+
     public int hashCode() {
-        return (gaurd.hashCode() +
-                truebranch.hashCode() +
-                falsebranch.hashCode());
+        return (guard.hashCode() +
+                trueBranch.hashCode() +
+                falseBranch.hashCode());
     }
+
     public String toString() {
         return ("IfStatement(" +
-                gaurd.toString() + ", " +
-                truebranch.toString() + ", " +
-                falsebranch.toString() + ")");
+                guard.toString() + ", " +
+                trueBranch.toString() + ", " +
+                falseBranch.toString() + ")");
     }
 }
