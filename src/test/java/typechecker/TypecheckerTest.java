54edaa46fe98e39c00d319aa03665ff182c6502f
RuitaoWu <ruitaowu@yahoo.com>
2022-04-18 10:55:25 -0700

    added default test cases for typechecker

---
+++ src/test/java/typechecker/TypecheckerTest.java
@@ -0,0 +1,45 @@
+package typechecker;
+
+import typechecker.parser.*;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+public class TypecheckerTest {
+    public static final Typechecker emptyTypechecker =
+        new Typechecker(new Program(new ArrayList<ClassDef>(),
+                                    new ExpStmt(new IntLiteralExp(0))));
+
+    @Test
+    public void testVariableInScope() throws TypeErrorException {
+        final Type expectedType = new IntType();
+        final Map<Var, Type> typeEnvironment = new HashMap<Var, Type>();
+        typeEnvironment.put(new Var("x"), new IntType());
+        
+        final Type receivedType =
+            emptyTypechecker.typeofVariable(new VarExp(new Var("x")),
+                                            typeEnvironment);
+        assertEquals(expectedType, receivedType);
+    }
+    
+    @Test(expected = TypeErrorException.class)
+    public void testVariableOutOfScope() throws TypeErrorException {
+        final Map<Var, Type> typeEnvironment = new HashMap<Var, Type>();
+        emptyTypechecker.typeofVariable(new VarExp(new Var("x")),
+                                        typeEnvironment);
+    }
+
+    @Test
+    public void testThisInClass() throws TypeErrorException {
+        assertEquals(new ClassNameType(new ClassName("foo")),
+                     emptyTypechecker.typeofThis(new ClassName("foo")));
+    }
+
+    @Test(expected = TypeErrorException.class)
+    public void testThisNotInClass() throws TypeErrorException {
+        emptyTypechecker.typeofThis(null);
+    }
+}
