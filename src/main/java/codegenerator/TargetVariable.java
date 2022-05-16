e2b57694afc289ddfa9f9420740504d18ab4265b
RuitaoWu <ruitaowu@yahoo.com>
2022-05-16 11:23:19 -0700

    added targetvariable java

--- src/main/java/codegenerator/TargetVariable.java
+++ src/main/java/codegenerator/TargetVariable.java
@@ -1,5 +1,18 @@
 package codegenerator;
 
 public class TargetVariable {
-    
+    public final String name;
+    public TargetVariable(String name) {
+        this.name = name;
+    }
+    public int hashCode() {
+        return name.hashCode();
+    }
+    public boolean equals(final Object others){
+        return (others instanceof TargetVariable) && 
+            name.equals(((TargetVariable)others).name);
+    }
+    public String toString() {
+        return "TargetVariable(" + name + ")";
+    }
 }
